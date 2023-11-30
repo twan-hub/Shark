b7da5e49b6ac57b0b49aacfe8c74ae91bda090f7
twan-hub <adstith99@gmail.com>
2023-11-29 17:02:38 -0800

    add  form inputs

--- src/pages/TodoAdd.tsx
+++ src/pages/TodoAdd.tsx
@@ -1,6 +1,6 @@
-import { IonBackButton, IonButtons, IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButton, IonButtons, IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import './TodoAdd.css';
 import { add, chevronUpCircle, colorPalette, globe } from 'ionicons/icons';
 
 const TodoAdd: React.FC = () => {
@@ -8,19 +8,31 @@ const TodoAdd: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-        <IonButtons slot="start">
-          <IonBackButton defaultHref="#"></IonBackButton>
-        </IonButtons>
-        <IonTitle>Back</IonTitle>
+          <IonTitle class="ion-text-center"> New Task </IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Todo</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Todo" />
+      <IonContent>
+        <div>
+          <IonInput label="New Task" labelPlacement="floating" maxlength={20}></IonInput>
+
+        <IonInput
+          label="Additional Detail (Optional) "
+          labelPlacement="floating"
+          maxlength={20}
+          counterFormatter={(inputLength, maxLength) => `${maxLength - inputLength} characters remaining`}
+        ></IonInput>
+        </div>
+        
+        <div>
+          <IonButtons>
+            <IonButton fill="clear" href="#">Back</IonButton>
+          </IonButtons>
+
+          <IonButtons>
+            <IonButton fill="clear" href="tab2">Save</IonButton>
+          </IonButtons>
+        </div>
+
       </IonContent>
     </IonPage>
   );
