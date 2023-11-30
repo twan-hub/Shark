7344e258535e30734cde83b9d724c9591d30f2a1
twan-hub <adstith99@gmail.com>
2023-11-29 19:07:37 -0800

    add validation to new task form

--- src/pages/TodoAdd.tsx
+++ src/pages/TodoAdd.tsx
@@ -1,23 +1,66 @@
-import { IonBackButton, IonButton, IonButtons, IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonInput, IonItem, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import './TodoAdd.css';
-import { add, chevronUpCircle, colorPalette, globe } from 'ionicons/icons';
+import { useHistory } from 'react-router';
 
 const TodoAdd: React.FC = () => {
+  const [newTask, setNewTask] = useState<string>('');
+  const [newDetails, setNewDetails] = useState<string>('');
+  const [formValid, setFormValid] = useState<boolean>(false);
+  const history = useHistory();
+
+  const handleTaskChange = (e: CustomEvent) => {
+    const value = (e.target as HTMLInputElement).value;
+    setNewTask(value);
+    validateForm(value, newDetails);
+  };
+
+  const handleDetailChange = (e: CustomEvent) => {
+    const value = (e.target as HTMLInputElement).value;
+    setNewDetails(value);
+    validateForm(newTask, value);
+  };
+
+  const validateForm = (newTask: string, newDetails: string) => {
+    if (newTask.trim() !== '' && newDetails.trim() !== '') {
+      setFormValid(true);
+    } else {
+      setFormValid(false);
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log('New Task:', newTask);
+    console.log('Task Details:', newDetails);
+    history.goBack();
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle class="ion-text-center"> New Task </IonTitle>
+          <IonTitle className="ion-text-center"> New Task </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <form className='ion-padding'>
+        <form onSubmit={handleSubmit} className="ion-padding">
           <IonItem>
             <IonInput
               label="New Task"
               labelPlacement="floating"
               maxlength={20}
+              onIonChange={handleTaskChange}
+              value={newTask}
             ></IonInput>
           </IonItem>
           <IonItem>
@@ -25,21 +68,32 @@ const TodoAdd: React.FC = () => {
               label="Additional Detail (Optional) "
               labelPlacement="floating"
               maxlength={20}
-              counterFormatter={(inputLength, maxLength) => `${maxLength - inputLength} characters remaining`}
+              counterFormatter={(inputLength, maxLength) =>
+                `${maxLength - inputLength} characters remaining`
+              }
+              onIonChange={handleDetailChange}
+              value={newDetails}
             ></IonInput>
           </IonItem>
-          <div className='form-buttons'>
-            <IonButtons>
-              <IonButton fill="clear" href="tab2">Back</IonButton>
+          <div className="form-buttons">
+            <IonButtons slot="start">
+              <IonButton fill="clear" href="tab2">
+                Back
+              </IonButton>
             </IonButtons>
 
-            <IonButtons>
-              <IonButton fill="clear" href="tab2">Save</IonButton>
+            <IonButtons slot="end">
+              <IonButton
+                type="submit"
+                fill="clear"
+                href="tab2"
+                disabled={!formValid}
+              >
+                Save
+              </IonButton>
             </IonButtons>
           </div>
         </form>
-
-
       </IonContent>
     </IonPage>
   );
