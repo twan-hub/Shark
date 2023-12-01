4d47ff39d2ec0eff9d36f7ef6cf21aa54fa3bdd8
twan-hub <adstith99@gmail.com>
2023-11-30 23:46:16 -0800

    add stlye and button to list

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,9 +1,9 @@
 // Tab2.tsx
 import React from 'react';
-import { IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { add } from 'ionicons/icons';
+import { IonAccordion, IonAccordionGroup, IonAvatar, IonButton, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { add, caretDownCircle, chevronForwardOutline, ellipseOutline, pin, share, trash } from 'ionicons/icons';
 import { useHistory } from 'react-router';
-
+import "./Tab2.css"
 const Tab2: React.FC = () => {
   const history = useHistory();
 
@@ -15,7 +15,7 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>To-Do List</IonTitle>
+          <IonTitle className='ion-text-center'>To-Do List</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -24,6 +24,22 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">To-Do List</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonList inset={true}>
+          <IonItemSliding>
+            <IonItem button={true}>
+              <IonIcon icon={ellipseOutline} style={{
+                paddingRight:10
+              }}/>
+              <IonLabel>Rick Astley</IonLabel>
+              <IonIcon icon={chevronForwardOutline}/>
+            </IonItem>
+            <IonItemOptions slot="end">
+              <IonItemOption color="danger" expandable={true}>
+                <IonIcon slot="icon-only" icon={trash}></IonIcon>
+              </IonItemOption>
+            </IonItemOptions>
+          </IonItemSliding>
+          </IonList>
         <IonFab slot="fixed" vertical="bottom" horizontal="end">
           <IonFabButton onClick={handleAddButtonClick}>
             <IonIcon icon={add} />
